6c8b46aecfe4a692f82b56b302d7b39b8f1bf6cf
sachin <sachinverma7.srg@gmail.com>
2025-03-15 20:40:22 +0530

    some classes change and add

--- my-projects/Area of triangle/script.js
+++ my-projects/Area of triangle/script.js
@@ -1,6 +1,8 @@
-let inp1 = document.querySelector(".input1");
-let inp2 = document.querySelector(".input2");
-let inp3 = document.querySelector(".input3");
+let inp1m1 = document.querySelector(".input1m-1");
+let inp2m1 = document.querySelector(".input2m-1");
+let inp1m2 = document.querySelector(".input1m-2");
+let inp2m2 = document.querySelector(".input2m-2");
+let inp3m2 = document.querySelector(".input3m-2");
 let btn1 = document.querySelector(".button1");
 let btn2 = document.querySelector(".button2");
 let btn3 = document.querySelector(".button3");
@@ -9,8 +11,8 @@ let rel2 = document.querySelector(".result2");
 let rel3 = document.querySelector(".result3");
 
 function area1() {
-  let x = inp1.value;
-  let y = inp2.value;
+  let x = inp1m1.value;
+  let y = inp2m1.value;
 
   console.log(x);
   console.log(y);
@@ -23,15 +25,22 @@ function area1() {
 btn1.addEventListener("click", area1);
 
 function area2() {
-  let x = inp1.value;
-  let y = inp2.value;
-  let z = inp3.value;
+  let x = inp1m2.value;
+  let y = inp2m2.value;
+  let z = inp3m2.value;
+  console.log(x);
+  console.log(y);
+  console.log(z);
 
   let w = x + y + z;
+  console.log("sum is", w);
   w = w / 2;
   console.log(w);
-  let k = w * (w - x)(w - y)(w - z);
+  let k = w * (w - x) * (w - y) * (w - z);
 
+  console.log(x);
+  console.log(y);
+  console.log(z);
   let q = Math.sqrt(k);
   console.log(q);
   rel2.innerText = ` Area = ${q}`;
