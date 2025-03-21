f34f20e2352975c6810f3ce00b64d094106c286e
sachin <sachinverma7.srg@gmail.com>
2025-03-21 20:02:30 +0530

    delet data of index , css and javascript file and update

--- my-projects/Calculator/script.js
+++ my-projects/Calculator/script.js
@@ -1,25 +1,4 @@
-let display = document.getElementById("display");
-
-// Append value to display
-function appendValue(value) {
-  display.value += value;
-}
-
-// Clear display
-function clearDisplay() {
-  display.value = "";
-}
-
-// Delete last character
-function deleteLast() {
-  display.value = display.value.slice(0, -1);
-}
-
-// Calculate result
-function calculateResult() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    alert("Invalid Calculation");
-  }
-}
+let inp1 = document.querySelector(".input1");
+let inp2 = document.querySelector(".input2");
+let btn = document.querySelector(".button");
+let rel = document.querySelector("result");
